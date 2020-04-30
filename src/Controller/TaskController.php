7b365aaef51260df0cc62a162dc53a8114eef80a
Cedric Flamain <cedflam@gmail.com>
2020-04-30 18:50:00 +0200

    Create task pages and user pages

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,18 +2,75 @@
 
 namespace App\Controller;
 
+use App\Entity\Task;
+use App\Form\TaskType;
+use App\Repository\TaskRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TaskController extends AbstractController
 {
     /**
-     * @Route("/task", name="task")
+     * Permet de récupérer toutesles tâches
+     *
+     * @Route("/tasks", name="task_list")
+     * @param TaskRepository $repo
+     * @return Response
      */
-    public function index()
+    public function taskAll(TaskRepository $repo)
     {
-        return $this->render('task/index.html.twig', [
-            'controller_name' => 'TaskController',
+        return $this->render('task/task-list.html.twig',[
+            'tasks'=> $repo->findBy(['isDone'=> false], ['createdAt'=>'DESC'])
         ]);
     }
+
+    /**
+     * Permet de récupérer la liste des tâches appartenant un à user
+     *
+     * @Route("/tasks/user", name="task_user_list")
+     * @param TaskRepository $repo
+     * @return Response
+     */
+    public function taskUser(TaskRepository $repo)
+    {
+        return $this->render('task/task-user-list.html.twig',[
+            'tasks'=> $repo->findBy(['user'=> $this->getUser()])
+        ]);
+
+    }
+
+    /**
+     * Permet de récupérer les tâches terminées
+     *
+     * @Route("/tasks/done", name="task_done_list")
+     * @param TaskRepository $repo
+     * @return Response
+     */
+    public function taskDone(TaskRepository $repo)
+    {
+        return $this->render('task/task-done-list.html.twig',[
+            'tasks'=> $repo->findBy(['isDone'=> true], ['createdAt'=>'DESC'])
+        ]);
+    }
+
+    /**
+     * Permet de créer une nouvelle tâche
+     *
+     * @Route("/task/new", name="task_new")
+     *
+     * @param Task $task
+     * @param EntityManagerInterface $manager
+     * @param Request $request
+     *
+     */
+    public function taskNew(Task $task, EntityManagerInterface $manager, Request $request)
+    {
+
+    }
+
+
 }
--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,7 +19,7 @@ class UserController extends AbstractController
     /**
      * Permet d'ajouter un utilisateur
      *
-     * @Route("/user/add", name="user_add")
+     * @Route("/user/create", name="user_create")
      *
      * @param Request $request
      * @param EntityManagerInterface $manager
@@ -39,11 +41,91 @@ class UserController extends AbstractController
             $manager->persist($user);
             $manager->flush();
 
+            $this->addFlash(
+                'success',
+                "L'utilisateur a été crée avec succès ! "
+            );
+
             return $this->redirectToRoute('homepage');
         }
 
-        return $this->render('user/add-user.html.twig',[
+        return $this->render('user/create-user.html.twig',[
             'form'=> $form->createView()
         ]);
     }
+
+    /**
+     * @Route("/user/show", name="users_show")
+     * @param UserRepository $repo
+     * @return Response
+     */
+    public function usersShow(UserRepository $repo)
+    {
+        return $this->render('user/show-users.html.twig', [
+            'users'=> $repo->findAll()
+        ]);
+    }
+
+    /**
+     * Permet de modifier un utilisateur
+     *
+     * @Route("/user/edit/{id}", name="user_edit")
+     *
+     * @param UserPasswordEncoderInterface $encoder
+     * @param User $user
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function userEdit(UserPasswordEncoderInterface $encoder,User $user, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $hash = $encoder->encodePassword($user, $user->getPassword());
+            $user->setPassword($hash);
+
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                    "L'utilisateur a été modifié avec succès !"
+            );
+
+            return $this->redirectToRoute('users_show');
+        }
+
+        return $this->render('user/edit-user.html.twig', [
+            'form'=> $form->createView(),
+            'user'=> $user
+        ]);
+    }
+
+    /**
+     * Permet de supprimer un utilisateur
+     *
+     * @Route("user/delete/{id}", name="user_delete")
+     *
+     * @param User $user
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    public function userDelete(User $user, EntityManagerInterface $manager)
+    {
+        $manager->remove($user);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'utilisateur a été supprimé avec succès !"
+        );
+
+        return $this->redirectToRoute('users_show');
+
+    }
+
+
 }
